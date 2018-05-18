3d0765c144a603b05ea8b87baa31b6b71a830cb2
lsponem <39214173+lsponem@users.noreply.github.com>
2018-05-18 09:31:10 +0200

    Update fonctions_jeu.py

--- fonctions_jeu.py
+++ fonctions_jeu.py
@@ -1,6 +1,6 @@
 """crée le 09/04/18 avec Python 3.2"""
-"""Module obstacle permettant les différentes actions
-sur les obstacles: création, déplacement et collision"""
+"""Module fonctions jeu permettant les différentes actions
+sur les obstacles, le personnage et les fonds: création, déplacement et collision"""
 
 from random import randint #importation des bibliothèques#
 import pygame 
@@ -52,24 +52,24 @@ def vitesse(pixels,score):
       pixels=3 #vitesse basse#
     if score>30:
       pixels=4
-    if score>50:
-      pixels=5
     if score>80:
+      pixels=5
+    if score>120:
       pixels=6
-    if score>100:
-      pixels=7
-    if score>150:
-      pixels=8
     if score>180:
+      pixels=7
+    if score>220:
+      pixels=8
+    if score>280:
       pixels=9
-    if score>210:
+    if score>340:
       pixels=10 
-    if score>300:  #score haut#
+    if score>400:  #score haut#
       pixels=11 #vitesse maximale#
     return(pixels)
 
 """fonction vérifiant la collision avec l'obstacle et la pièce"""
-"""predn comme paramètres la position de l'objet et du personnage et renvoie crash"""
+"""prend comme paramètres la position de l'objet et du personnage et renvoie crash"""
 
 def crash_test(objet,perso,crash):
     if objet.colliderect(perso):   #vérification de la collision entre le personnage et l'objet#
@@ -117,40 +117,46 @@ def defilement_piece(position_piece):
 """prend pour paramètres le personnage et sa position"""
 
 def haut(position_perso):
-  position_perso=position_perso.move(0,-6) #modification de l'ordonnée du personnage#
+  position_perso=position_perso.move(0,-5) #modification de l'ordonnée du personnage#
   return(position_perso) #renvoie la nouvelle position#
 
 """fonction faisant redescendre le personnage"""
 """prend pour paramètres le personnage et sa position"""
 
 def bas(position_perso):
-  position_perso=position_perso.move(0,6) #modification de l'ordonnée du personnage#
+  position_perso=position_perso.move(0,5) #modification de l'ordonnée du personnage#
   return(position_perso) #renvoie la nouvelle position#
 
 """fonction déplaçant le personnage vers la droite"""
 """prend comme paramètres le personnage et sa position"""
 
 def droite(position_perso):
-  position_perso=position_perso.move(6,0) #modification de l'abscisse#
+  position_perso=position_perso.move(5,0) #modification de l'abscisse#
   return(position_perso) #renvoie la nouvelle position#
 
 """fonction déplaçant le personnage vers la gauche"""
 """prend comme paramètres le personnage et sa position"""
 
 def gauche(position_perso):
-  position_perso=position_perso.move(-6,0) #modification de l'abscisse#
+  position_perso=position_perso.move(-5,0) #modification de l'abscisse#
   return(position_perso) #renvoie la nouvelle position#
 
+"""fonction permettant le défilement du fond vers la droite"""
+"""prend comme paramètre les positions des trois images du fond et les renvoi"""
+
 def defilement_fond_droite(position_fond,position_fond1,position_fond2):
-  position_fond=position_fond.move((5/2),0)
+  position_fond=position_fond.move((5/2),0) #augmentation de 2,5 pixels de l'abscisse de l'image#
   position_fond1=position_fond1.move((5/2),0)
   position_fond2=position_fond2.move((5/2),0)
-  return(position_fond,position_fond1,position_fond2)
+  return(position_fond,position_fond1,position_fond2) #renvoie les nouvelles positions#
+
+"""fonction permettant le défilement du fond vers la gauche"""
+"""prend comme paramètre les positions des trois images du fond et les renvoi"""
 
 def defilement_fond_gauche(position_fond,position_fond1,position_fond2):
-  position_fond=position_fond.move((-5/2),0)
+  position_fond=position_fond.move((-5/2),0) #diminution de 2,5 pixels de l'abscisse de l'image#
   position_fond1=position_fond1.move((-5/2),0)
   position_fond2=position_fond2.move((-5/2),0)
-  return(position_fond,position_fond1,position_fond2)
+  return(position_fond,position_fond1,position_fond2) #renvoie les nouvelles positions#
   
     
